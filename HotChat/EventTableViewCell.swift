1dc50bf459a803e56f573b6fa41f7a0cc3625bc5
佐藤 俊輔 <shunsuke.sato@20201446n.intra.emercury.co.jp>
2016-04-20 11:45:15 +0900

    add some files (empty files)

---
+++ HotChat/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  HotChat
+//
+//  Created by 佐藤 俊輔 on 2016/04/20.
+//  Copyright © 2016年 moguraproject. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HotChat/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  HotChat
+//
+//  Created by 佐藤 俊輔 on 2016/04/20.
+//  Copyright © 2016年 moguraproject. All rights reserved.
+//
+
+import Foundation

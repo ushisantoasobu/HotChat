728cc5bf8ed0cf9d8478bbf04490934b84e6338e
佐藤 俊輔 <shunsuke.sato@20201446n.intra.emercury.co.jp>
2016-04-20 11:33:39 +0900

    create XCode project

---
+++ HotChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotChat
+//
+//  Created by 佐藤 俊輔 on 2016/04/20.
+//  Copyright © 2016年 moguraproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

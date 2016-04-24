eab3500794bfdb91d96f1c7fb916961941c286e2
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-04-24 10:49:52 +0900

    [ReSwift] create empty AppState

---
+++ HotChat/AppState.swift
@@ -0,0 +1,14 @@
+//
+//  AppState.swift
+//  HotChat
+//
+//  Created by SatoShunsuke on 2016/04/24.
+//  Copyright © 2016年 moguraproject. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+struct AppState: StateType {
+    //
+}

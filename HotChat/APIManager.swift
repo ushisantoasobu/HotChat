0710d7d2aedb103df4dd9d3b8b6cea96ebb85e42
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-04-24 15:46:18 +0900

    add dummy delay for API response

--- HotChat/APIManager.swift
+++ HotChat/APIManager.swift
@@ -26,24 +26,32 @@ class APIManager {
     }
     
     func getEvents(location :Location, handler : [Event] -> Void ) {
+        let delay = 1.2 * Double(NSEC_PER_SEC)
+        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue(), {
 
-        var event1 = Event()
-        event1.name = "Sun kil moon 単独公演"
-        event1.chatCount = 3
+            var event1 = Event()
+            event1.name = "Sun kil moon 単独公演"
+            event1.chatCount = 3
 
-        var event2 = Event()
-        event2.name = "Spiritualized - AcousticMainlines"
-        event2.chatCount = 8
-
-        handler([event1, event2])
+            var event2 = Event()
+            event2.name = "Spiritualized - AcousticMainlines"
+            event2.chatCount = 8
+            
+            handler([event1, event2])
+        })
     }
     
     func getEvents(userId :Int, handler :([Event] -> Void)) {
-        var event1 = Event()
-        event1.name = "ドリップコーヒー選手権 2016"
-        event1.chatCount = 34
+        let delay = 0.4 * Double(NSEC_PER_SEC)
+        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            var event1 = Event()
+            event1.name = "ドリップコーヒー選手権 2016"
+            event1.chatCount = 34
 
-        handler([event1])
+            handler([event1])
+        })
     }
     
     func getEvent(eventId :Int) -> Event? {
@@ -52,28 +60,26 @@ class APIManager {
 
     func getChats(eventId :Int, handler :([Chat] -> Void)){
 
-//        let delay = 1.0 * Double(NSEC_PER_SEC)
-//        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-//        dispatch_after(time, dispatch_get_main_queue(), {
-//
-//        })
+        let delay = 1.0 * Double(NSEC_PER_SEC)
+        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            var user1 = User()
+            user1.name = "ushisantoasobu"
 
-        var user1 = User()
-        user1.name = "ushisantoasobu"
+            var chat1 = Chat()
+            chat1.user = user1
+            chat1.message = "楽しみですね、よろしくお願いします！！"
+            chat1.mine = true
 
-        var chat1 = Chat()
-        chat1.user = user1
-        chat1.message = "楽しみですね、よろしくお願いします！！"
-        chat1.mine = true
+            var user2 = User()
+            user2.name = "高畑充希"
 
-        var user2 = User()
-        user2.name = "高畑充希"
-
-        var chat2 = Chat()
-        chat2.user = user2
-        chat2.message = "全然人入ってない > < 見やすいけど寂しい"
-        chat2.mine = false
-
-        handler([chat1, chat2])
+            var chat2 = Chat()
+            chat2.user = user2
+            chat2.message = "全然人入ってない > < 見やすいけど寂しい"
+            chat2.mine = false
+            
+            handler([chat1, chat2])
+        })
     }
 }
